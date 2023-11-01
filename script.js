e94bd26f7b30f360dc4770a3a046e84219a226c3
orbitze <meghavx@outlook.com>
2023-11-02 03:07:40 +0530

    refactor script.js

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
-function padded(item) {
-    return `${item < 10 ? "0" : ""}${item}`;
+// Utility function
+function pad(unit) {
+    return `${unit < 10 ? "0" : ""}${unit}`;
 }
 
-function displayTime() {
+function displayTimeAndDate() {
     let timeDate = new Date();
     
     // Extract date 
-    let mmm = timeDate.toLocaleString("default", { month: "short" })
-    let dd = timeDate.getDate();
+    let mmm  = timeDate.toLocaleString("default", { month: "short" })
+    let dd   = timeDate.getDate();
     let yyyy = timeDate.getFullYear();
 
-    // Construct and update date
-    let date = `${mmm} ${padded(dd)}, ${yyyy}`;
+    // Update date
+    let date = `${mmm} ${pad(dd)}, ${yyyy}`;
     document.getElementById("date").innerHTML = date;
 
     // Extract time
     let hh = timeDate.getHours();
     let mm = timeDate.getMinutes();
     let ss = timeDate.getSeconds();
-    let session = hh < 12 ? "am" : "pm";  
+    let period = hh < 12 ? "am" : "pm";  
     
     // Convert hours according to the 12-hour system
     hh = hh > 12 ? hh - 12 : hh;
 
-    // Construct and update time
-    let time = `${padded(hh)}:${padded(mm)}:${padded(ss)} ${session}`;
+    // Update time
+    let time = `${pad(hh)}:${pad(mm)}:${pad(ss)} ${period}`;
     document.getElementById("time").innerHTML = time;
 }
 
-setInterval(displayTime, 1);
+// Function call
+setInterval(displayTimeAndDate, 1);
